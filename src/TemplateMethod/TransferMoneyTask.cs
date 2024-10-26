d650f9252626b08d79fa2494096de7c7b6f1d257
Musa Alp <Alp@pre-sustainability.com>
2024-10-26 20:35:06 +0200

    feat: Improve implementation of the Template Method pattern

--- src/TemplateMethod/TransferMoneyTask.cs
+++ src/TemplateMethod/TransferMoneyTask.cs
@@ -6,6 +6,10 @@ namespace TemplateMethodDesignPattern
 {
     public class TransferMoneyTask : Task
     {
+        public TransferMoneyTask(AuditTrail auditTrail) : base(auditTrail)
+        {
+        }
+
         protected override void DoExecute()
         {
             Console.WriteLine("Transfer Money");
